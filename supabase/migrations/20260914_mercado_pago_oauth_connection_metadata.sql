345ae18c931dfc6d6e46d9081eac449cf486369e
Cledemilson Oliveira de Assis <consultorcledemilsonoliveira@gmail.com>
2026-09-14 13:15:50 -0300

    docs(db): documentar conexoes OAuth e plataforma Mercado Pago

---
+++ supabase/migrations/20260914_mercado_pago_oauth_connection_metadata.sql
@@ -0,0 +1,2 @@
+comment on column public.payment_provider_connections.connection_type is
+  'oauth = conta Mercado Pago individual autorizada pelo vendedor; platform = credencial da Conta Dono/plataforma.';
